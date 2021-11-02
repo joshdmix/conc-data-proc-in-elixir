5dd22f91ad9def090d431cab107cd8f681b7e658
Josh Mix <josh.mix@blockfi.com>
2021-11-02 14:31:16 -0400

    pg 82 - add OnlinePageProducerConsumer

--- lib/scraper.ex
+++ lib/scraper.ex
@@ -2,4 +2,10 @@ defmodule Scraper do
   def work() do
     1..5 |> Enum.random() |> :timer.seconds() |> Process.sleep()
   end
+
+  def online?(_url) do
+    work()
+
+    Enum.random([false, true, true])
+  end
 end
